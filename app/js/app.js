1a53e732b7f14891b208eab1ce028166009d318d
Richard Nevin <richard.nevin@outlook.com>
2014-03-26 19:03:25 +0000

    Initial Setup

---
+++ app/js/app.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function() {
+    $("#convertbutton").click(function() {
+        var metricValue = $("#metricinput").val();
+        var imperialValue = $("#imperialinput").val();
+
+
+        if (((metricValue.length !=0) && (imperialValue.length !=0))
+            || ((metricValue.length == 0) && imperialValue.length == 0)) {
+            alert("Please set one field to convert")
+        }
+        if (metricValue.length != 0) {
+            $("#imperialinput").val(Converter.convertFromPoundsToKIlo(metricValue));
+        }
+        else if (imperialValue.length != 0) {
+            $("#metricinput").val(Converter.convertFromMetricToImperial(imperialValue));
+        }
+    });
+    $("#clearbutton").click(function() {
+        $("#imperialinput").val('');
+        $("#metricinput").val('');
+    });
+
+});
